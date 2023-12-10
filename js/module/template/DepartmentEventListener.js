92ccf754983c8f6c08da6b727d2fa7cc11a112f0
firsachi <firsachi.alex@gmail.com>
2023-12-10 18:05:24 +0200

    pzd

---
+++ js/module/template/DepartmentEventListener.js
@@ -0,0 +1,17 @@
+import { getDepartment } from "../api/DepartmentApi";
+
+function departmentEventListener(link) {
+    link.addEventListener("click", async function(){
+        const departmentId = this.getAttribute("data-department");
+        const companyId = this.getAttribute("data-comapny");
+        const department = await getDepartment(departmentId);
+        fillHeaderEmployee(department);
+        const listEmployee = await getListEmployee(companyId, departmentId);
+        renderEmployeeItem(listEmployee);
+        $('div#menuDepartment > a.callout.alert-callout-border').removeClass("primary");
+        this.classList.add("primary");
+    });
+    return link;
+};
+
+export{ departmentEventListener };
